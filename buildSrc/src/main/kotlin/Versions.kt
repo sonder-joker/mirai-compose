27fbf77df7a659581fbf07053433a17b5cec2eab
younger <highmagic@outlook.com>
2021-04-11 23:31:12 +0800

    Update dependency

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,12 +3,12 @@
 object Versions {
     const val mirai_compose = "1.1.1"
 
-    const val kotlin = "1.4.31"
+    const val kotlin = "1.4.32"
 
     const val mirai = "2.5.0"
 
-    const val compose = "0.3.2"
-    const val decompose = "0.1.9"
+    const val compose = "0.4.0-build180"
+    const val decompose = "0.2.1"
 
     const val yamlkt = "0.7.5"
 
