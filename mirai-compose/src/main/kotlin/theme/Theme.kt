e065ce05648741bb1d5004fff68cb9d53980f5f0
younger <highmagic@outlook.com>
2021-04-13 19:30:51 +0800

    Need report issue later but current merge

--- mirai-compose/src/main/kotlin/theme/Theme.kt
+++ mirai-compose/src/main/kotlin/theme/Theme.kt
@@ -1,172 +1,108 @@
-@file:UseSerializers(ColorSerializer::class, ColorsSerializer::class)
-
 package com.youngerhousea.miraicompose.theme
 
 import androidx.compose.material.Colors
 import androidx.compose.material.lightColors
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.KSerializer
+import com.youngerhousea.miraicompose.console.ComposeDataScope
+import com.youngerhousea.miraicompose.utils.ColorSerializer
+import com.youngerhousea.miraicompose.utils.ColorsSerializer
+import com.youngerhousea.miraicompose.utils.MutableStateSerializer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.UseSerializers
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.descriptors.buildClassSerialDescriptor
-import kotlinx.serialization.encoding.*
 import net.mamoe.mirai.console.data.AutoSavePluginConfig
 import net.mamoe.mirai.console.data.value
 
 
-@Suppress("NOTHING_TO_INLINE")
-private inline fun Int.toHexString(): String = Integer.toHexString(this)
-
-internal object ColorSerializer : KSerializer<Color> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Color", PrimitiveKind.STRING)
-
-    override fun serialize(encoder: Encoder, value: Color) {
-        encoder.encodeString("#${value.toArgb().toHexString()}")
-    }
-
-    override fun deserialize(decoder: Decoder): Color =
-        Color(decoder.decodeString().removePrefix("#").toLong(16))
-}
-
-internal object ColorsSerializer : KSerializer<Colors> {
-    override val descriptor: SerialDescriptor =
-        buildClassSerialDescriptor("Colors") {
-            element("primary", ColorSerializer.descriptor)
-            element("primaryVariant", ColorSerializer.descriptor)
-            element("secondary", ColorSerializer.descriptor)
-            element("secondaryVariant", ColorSerializer.descriptor)
-            element("background", ColorSerializer.descriptor)
-            element("surface", ColorSerializer.descriptor)
-            element("error", ColorSerializer.descriptor)
-            element("onPrimary", ColorSerializer.descriptor)
-            element("onSecondary", ColorSerializer.descriptor)
-            element("onBackground", ColorSerializer.descriptor)
-            element("onSurface", ColorSerializer.descriptor)
-            element("onError", ColorSerializer.descriptor)
-            element("isLight", ColorSerializer.descriptor)
-        }
-
-    override fun serialize(encoder: Encoder, value: Colors) =
-        encoder.encodeStructure(descriptor) {
-            encodeSerializableElement(descriptor, 0, ColorSerializer, value.primary)
-            encodeSerializableElement(descriptor, 1, ColorSerializer, value.primaryVariant)
-            encodeSerializableElement(descriptor, 2, ColorSerializer, value.secondary)
-            encodeSerializableElement(descriptor, 3, ColorSerializer, value.secondaryVariant)
-            encodeSerializableElement(descriptor, 4, ColorSerializer, value.background)
-            encodeSerializableElement(descriptor, 5, ColorSerializer, value.surface)
-            encodeSerializableElement(descriptor, 6, ColorSerializer, value.error)
-            encodeSerializableElement(descriptor, 7, ColorSerializer, value.onPrimary)
-            encodeSerializableElement(descriptor, 8, ColorSerializer, value.onSecondary)
-            encodeSerializableElement(descriptor, 9, ColorSerializer, value.onBackground)
-            encodeSerializableElement(descriptor, 10, ColorSerializer, value.onSurface)
-            encodeSerializableElement(descriptor, 11, ColorSerializer, value.onError)
-            encodeBooleanElement(descriptor, 12, value.isLight)
-        }
-
-
-    @OptIn(ExperimentalSerializationApi::class)
-    override fun deserialize(decoder: Decoder): Colors =
-        decoder.decodeStructure(descriptor) {
-            var a0 = Color(0)
-            var a1 = Color(0)
-            var a2 = Color(0)
-            var a3 = Color(0)
-            var a4 = Color(0)
-            var a5 = Color(0)
-            var a6 = Color(0)
-            var a7 = Color(0)
-            var a8 = Color(0)
-            var a9 = Color(0)
-            var a10 = Color(0)
-            var a11 = Color(0)
-            var a12 = false
-            if (decodeSequentially()) {
-                a0 = decodeSerializableElement(descriptor, 0, ColorSerializer)
-                a1 = decodeSerializableElement(descriptor, 1, ColorSerializer)
-                a2 = decodeSerializableElement(descriptor, 2, ColorSerializer)
-                a3 = decodeSerializableElement(descriptor, 3, ColorSerializer)
-                a4 = decodeSerializableElement(descriptor, 4, ColorSerializer)
-                a5 = decodeSerializableElement(descriptor, 5, ColorSerializer)
-                a6 = decodeSerializableElement(descriptor, 6, ColorSerializer)
-                a7 = decodeSerializableElement(descriptor, 7, ColorSerializer)
-                a8 = decodeSerializableElement(descriptor, 8, ColorSerializer)
-                a9 = decodeSerializableElement(descriptor, 9, ColorSerializer)
-                a10 = decodeSerializableElement(descriptor, 10, ColorSerializer)
-                a11 = decodeSerializableElement(descriptor, 11, ColorSerializer)
-                a12 = decodeBooleanElement(descriptor, 12)
-            } else while (true) {
-                when (val index = decodeElementIndex(descriptor)) {
-                    0 -> a0 = decodeSerializableElement(descriptor, 0, ColorSerializer)
-                    1 -> a1 = decodeSerializableElement(descriptor, 1, ColorSerializer)
-                    2 -> a2 = decodeSerializableElement(descriptor, 2, ColorSerializer)
-                    3 -> a3 = decodeSerializableElement(descriptor, 3, ColorSerializer)
-                    4 -> a4 = decodeSerializableElement(descriptor, 4, ColorSerializer)
-                    5 -> a5 = decodeSerializableElement(descriptor, 6, ColorSerializer)
-                    6 -> a6 = decodeSerializableElement(descriptor, 6, ColorSerializer)
-                    7 -> a7 = decodeSerializableElement(descriptor, 7, ColorSerializer)
-                    8 -> a8 = decodeSerializableElement(descriptor, 8, ColorSerializer)
-                    9 -> a9 = decodeSerializableElement(descriptor, 9, ColorSerializer)
-                    10 -> a10 = decodeSerializableElement(descriptor, 10, ColorSerializer)
-                    11 -> a11 = decodeSerializableElement(descriptor, 11, ColorSerializer)
-                    12 -> a12 = decodeBooleanElement(descriptor, 12)
-                    CompositeDecoder.DECODE_DONE -> break
-                    else -> error("Unexpected index: $index")
-                }
-            }
-            Colors(
-                a0,
-                a1,
-                a2,
-                a3,
-                a4,
-                a5,
-                a6,
-                a7,
-                a8,
-                a9,
-                a10,
-                a11,
-                a12
-            )
-        }
-}
-
 @Serializable
 internal class LogColor {
-    var verbose: Color = Color(0xFF00FF00)
-    var info: Color = Color(0xFF00FF00)
-    var warning: Color = Color(0xFFFFFF00)
-    var error: Color = Color(0xFFFFFF00)
-    var debug: Color = Color(0xFFCCCCCC)
+    @SerialName("debug")
+    @Serializable(ColorSerializer::class)
+    private var _debug: Color = Color(0xCCCCCC)
+
+    @SerialName("verbose")
+    @Serializable(ColorSerializer::class)
+    private var _verbose: Color = Color(0x00FF00)
+
+//    @SerialName("verbose")
+//    @Serializable(MutableStateSerializer::class)
+//    private var _verbose: MutableState<@Serializable(ColorSerializer::class) Color> = mutableStateOf(Color(0x00FF00))
+
+    @SerialName("info")
+    @Serializable(ColorSerializer::class)
+    private var _info: Color = Color(0x00FF00)
+
+    @SerialName("warning")
+    @Serializable(ColorSerializer::class)
+    private var _warning: Color = Color(0xFFFF00)
+
+    @SerialName("error")
+    @Serializable(ColorSerializer::class)
+    private var _error: Color = Color(0xFFFF00)
+
+    var debug: Color
+        get() = _debug
+        set(value) {
+            saveSetting { _debug = value }
+        }
+
+    var verbose: Color
+        get() = _verbose
+        set(value) {
+            saveSetting { _verbose = value }
+        }
+
+    var info: Color
+        get() = _info
+        set(value) {
+            saveSetting { _info = value }
+        }
+
+    var warning: Color
+        get() = _warning
+        set(value) {
+            saveSetting { _warning = value }
+        }
+
+    var error: Color
+        get() = _error
+        set(value) {
+            saveSetting { _error = value }
+        }
+
 }
 
 @Serializable
 internal class AppTheme {
-    private val _materialLight = mutableStateOf(
+    @SerialName("materialLight")
+    @Serializable(MutableStateSerializer::class)
+    private val _materialLight: MutableState<@Serializable(ColorsSerializer::class) Colors> = mutableStateOf(
         lightColors(
-            // 目前为头像默认颜色
-            surface = Color.Black,
-            // 输入框背景颜色
-            onSurface = Color.Gray,
-            // 主页面侧边栏颜色
-            primary = Color(211, 211, 211),
-            // 文字颜色
-            onPrimary = Color.White
+            surface = Color(0xff979595),
+            onSurface = Color.White,
+            primary = Color(0xf5f5f5f5),
+            onPrimary = Color.Black
         )
     )
 
     val logColors: LogColor = LogColor()
-    var materialLight: Colors by _materialLight
+
+    val materialLight: Colors
+        get() = _materialLight.value
 }
+
 internal object ComposeSetting : AutoSavePluginConfig("ComposeSetting") {
-    val AppTheme by value(AppTheme())
+
+    val AppTheme: AppTheme by value(AppTheme())
 }
+
+private fun saveSetting(block: suspend CoroutineScope.() -> Unit) =
+    ComposeDataScope.launch(block = block)
+
+
+
+
 
--- mirai-compose/src/main/kotlin/ui/feature/setting/SettingView.kt
+++ mirai-compose/src/main/kotlin/ui/feature/setting/SettingView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Button
+import androidx.compose.material.SliderDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
@@ -15,28 +16,26 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.shortcuts
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.arkivanov.decompose.ComponentContext
-import com.youngerhousea.miraicompose.console.ComposeDataScope
 import com.youngerhousea.miraicompose.theme.ComposeSetting
+import com.youngerhousea.miraicompose.utils.IntSlider
 import com.youngerhousea.miraicompose.utils.getARGB
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import java.util.*
 
 @Suppress("NOTHING_TO_INLINE")
-private inline fun String.toColor():Color = run {
+private inline fun String.toColor(): Color = run {
     // r, g, b or a, r, g, b
-    val tmp:List<Int> =
-        if(this.contains(',')) {
+    val tmp: List<Int> =
+        if (this.contains(',')) {
             this.split(',').map { it.toInt() }
-        }
-        else if (this.startsWith('#')) {
+        } else if (this.startsWith('#')) {
             getARGB(this.removePrefix("#")).toList()
-        }else{
+        } else {
             throw InputMismatchException()
         }
     return if (tmp.count() == 4) {
@@ -73,42 +72,7 @@ fun SettingUi() {
                     .fillMaxWidth()
                     .animateContentSize()
             ) {
-                SimpleSetWindows("VERBOSE", ComposeSetting.AppTheme.logColors.verbose) {
-                    ComposeSetting.AppTheme.logColors.verbose = it.toColor()
-                }
-                SimpleSetWindows("INFO", ComposeSetting.AppTheme.logColors.info) {
-                    ComposeSetting.AppTheme.logColors.info = it.toColor()
-                }
-                SimpleSetWindows("WARING", ComposeSetting.AppTheme.logColors.warning) {
-                    ComposeSetting.AppTheme.logColors.warning = it.toColor()
-                }
-                SimpleSetWindows("ERROR", ComposeSetting.AppTheme.logColors.error) {
-                    ComposeSetting.AppTheme.logColors.error = it.toColor()
-                }
-                SimpleSetWindows("DEBUG", ComposeSetting.AppTheme.logColors.debug) {
-                    ComposeSetting.AppTheme.logColors.debug = it.toColor()
-                }
-                SimpleSetWindows("主题颜色", ComposeSetting.AppTheme.materialLight.primary) {
-                    ComposeSetting.AppTheme.materialLight =
-                        ComposeSetting.AppTheme.materialLight
-                            .copy(primary = it.toColor())
-                }
-                SimpleSetWindows("文字颜色", ComposeSetting.AppTheme.materialLight.onPrimary) {
-                    ComposeSetting.AppTheme.materialLight =
-                        ComposeSetting.AppTheme.materialLight
-                            .copy(onPrimary = it.toColor())
-                }
-                SimpleSetWindows("输入框背景颜色", ComposeSetting.AppTheme.materialLight.onSurface) {
-                    ComposeSetting.AppTheme.materialLight =
-                        ComposeSetting.AppTheme.materialLight
-                            .copy(onSurface = it.toColor())
-
-                }
-                SimpleSetWindows("头像默认颜色", ComposeSetting.AppTheme.materialLight.surface) {
-                    ComposeSetting.AppTheme.materialLight =
-                        ComposeSetting.AppTheme.materialLight
-                            .copy(surface = it.toColor())
-                }
+                SlideSetWindow()
             }
         }
         VerticalScrollbar(
@@ -120,11 +84,36 @@ fun SettingUi() {
     }
 }
 
+
+@Composable
+fun SlideSetWindow() {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp)
+            .height(40.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text("Verbose")
+        IntSlider(
+            value = ComposeSetting.AppTheme.logColors.verbose.toArgb(),
+            onValueChange = {
+                println(it)
+                ComposeSetting.AppTheme.logColors.verbose = Color(it)
+            },
+            valueRange = 0..0xffffff,
+            colors = SliderDefaults.colors(thumbColor = ComposeSetting.AppTheme.logColors.verbose)
+        )
+    }
+}
+
 @Composable
 private fun SimpleSetWindows(textValue: String, color: Color, action: (value: String) -> Unit) {
     var textFieldValue by remember(textValue) { mutableStateOf("") }
     var errorTip by remember(textValue) { mutableStateOf("") }
     var isError by remember(textValue) { mutableStateOf(false) }
+
     Row(
         Modifier
             .fillMaxWidth()
@@ -148,40 +137,42 @@ private fun SimpleSetWindows(textValue: String, color: Color, action: (value: St
                 .weight(2f)
                 .padding(end = 20.dp)
                 .shortcuts {
-                           on(Key.Enter){
-                               //TODO click the button
-                           }
+                    on(Key.Enter) {
+                        //TODO click the button
+                    }
                 },
             isError = isError,
             label = {
                 Text(errorTip)
             }
         )
+
         Button({
-            ComposeDataScope.launch {
-                runCatching {
-                    action(textFieldValue)
-                }.onFailure {
-                    errorTip = if (it is NumberFormatException) {
+            runCatching {
+                action(textFieldValue)
+            }.onFailure {
+                errorTip = when (it) {
+                    is NumberFormatException -> {
                         "Wrong string"
-                    }else if(it is InputMismatchException){
+                    }
+                    is InputMismatchException -> {
                         "Wrong formation"
                     }
-                    else {
+                    else -> {
                         "Unknown string"
                     }
-                    isError = true
-                    delay(3 * 1000)
-                    isError = false
-                    errorTip = ""
-                }.onSuccess {
-                    // TODO change the color of tip to green
-                    errorTip = "Success"
-                    isError = true
-                    delay(3 * 1000)
-                    isError = false
-                    errorTip = ""
                 }
+//                isError = true
+//                delay(1000)
+//                isError = false
+//                errorTip = ""
+            }.onSuccess {
+                // TODO change the color of tip to green
+//                errorTip = "Success"
+//                isError = true
+//                delay(1000)
+//                isError = false
+//                errorTip = ""
             }
         }) { Text("修改") }
     }
