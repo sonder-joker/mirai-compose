338a46dc8f53bb38165830534f75b60c326e963b
sonder-joker <highmagic@outlook.com>
2021-12-03 00:43:26 +0800

    [Dep] Update to compose-jb v1.0 , remove unnecessary ExceptionWindow.kt

--- app/src/main/kotlin/EntryPoint.kt
+++ app/src/main/kotlin/EntryPoint.kt
@@ -11,16 +11,12 @@ import com.youngerhousea.mirai.compose.console.Login
 import com.youngerhousea.mirai.compose.console.LoginSolverState
 import com.youngerhousea.mirai.compose.console.MiraiComposeImplementation
 import com.youngerhousea.mirai.compose.console.impl.MiraiComposeImpl
-import com.youngerhousea.mirai.compose.ui.ExceptionWindow
 import com.youngerhousea.mirai.compose.ui.HostPage
 import com.youngerhousea.mirai.compose.ui.login.PicCaptchaDialog
 import com.youngerhousea.mirai.compose.ui.login.SliderCaptchaDialog
 import com.youngerhousea.mirai.compose.ui.login.UnsafeDeviceLoginVerifyDialog
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.flow.MutableStateFlow
-import net.mamoe.mirai.console.MiraiConsole
+import net.mamoe.mirai.console.ConsoleFrontEndImplementation
 import net.mamoe.mirai.console.MiraiConsoleImplementation.Companion.start
-
 
 fun main() = miraiComposeApplication {
     MiraiComposeWindow(onCloseRequest = ::exitApplication) {
@@ -31,7 +27,7 @@ fun main() = miraiComposeApplication {
 private inline fun miraiComposeApplication(crossinline content: @Composable ApplicationScope.() -> Unit) {
     val compose = MiraiComposeImpl()
     compose.start()
-    handExceptionApplication {
+    themeApplication {
         CompositionLocalProvider(LocalMiraiCompose provides compose) {
             CompositionLocalProvider(LocalViewModelStore provides compose.viewModelStore) {
                 LoginSolverDialog()
@@ -64,32 +60,6 @@ private fun LoginSolverDialog() {
     }
 }
 
-private inline fun handExceptionApplication(crossinline content: @Composable ApplicationScope.() -> Unit) {
-    val exceptionWindows = MutableStateFlow(false)
-    val exceptionMessage: MutableStateFlow<Throwable?> = MutableStateFlow(null)
-
-    Thread.setDefaultUncaughtExceptionHandler { _: Thread, e: Throwable ->
-        println(e.stackTraceToString())
-        exceptionWindows.value = true
-        exceptionMessage.value = e
-        if (MiraiConsole.isActive) {
-            MiraiConsole.cancel()
-        }
-    }
-
-    themeApplication {
-        val isException by exceptionWindows.collectAsState()
-        val message by exceptionMessage.collectAsState()
-        ExceptionWindow(
-            onCloseRequest = ::exitApplication,
-            visible = isException,
-            message?.stackTraceToString() ?: "No Error Found?"
-        )
-        content()
-    }
-}
-
-
 fun themeApplication(
     content: @Composable ApplicationScope.() -> Unit
 ) {
@@ -97,7 +67,6 @@ fun themeApplication(
         MaterialTheme(colors = color) {
             content()
         }
-
     }
 }
 
