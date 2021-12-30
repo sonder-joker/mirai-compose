d89d1851bc3f6ead94f9bc88cd4898fd0a680e51
sonder-joker <highmagic@outlook.com>
2021-12-30 22:54:43 +0800

    [Release] release v1.1.2

--- buildSrc/src/main/kotlin/MiraiCompose.kt
+++ buildSrc/src/main/kotlin/MiraiCompose.kt
@@ -1,7 +1,7 @@
 object MiraiCompose {
     const val name = "mirai-compose"
     const val group = "com.youngerhousea.mirai"
-    const val version = "1.1.1"
+    const val version = "1.1.2"
     const val mainClass = "com.youngerhousea.mirai.compose.EntryPointKt"
     const val windowsUUID = "01BBD7BE-A84F-314A-FA84-67B63728A416"
 }
